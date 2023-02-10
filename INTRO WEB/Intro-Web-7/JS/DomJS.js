d233c9e6282664fe7ef64b3d73d527fa8ef0ae0a
leondealex <deleonavalosa@gmail.com>
2023-02-10 01:16:14 -0600

    Se agregan archivos

---
+++ INTRO WEB/Intro-Web-7/JS/DomJS.js
@@ -0,0 +1,4 @@
+function evento (){
+    document.write("Hola esta es una introducción al JS con Funciones y Dom")
+    alert("Ya fuimos al pipirin")
+}
---
+++ JS01/tiposdedatosyvariables.js
@@ -0,0 +1,20 @@
+
+var nombre= "Fernanda";
+var edad=30;
+var tengomascotas=true;
+var tengotrabajo=false;
+var tengosueldo;//undefinied
+
+
+let nombre= "Fernanda";
+let edad=30;
+let tengomascotas=true;
+let tengotrabajo=false;
+let tengosueldo;//undefinied
+
+
+
+
+
+
+
