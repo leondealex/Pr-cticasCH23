ea20777196aa459e88b6216a58fd5d3128c38d3b
leondealex <deleonavalosa@gmail.com>
2023-02-23 17:39:01 -0600

    Java

---
+++ JAVA/Java-01/Java-Terminal/Saludo.java
@@ -0,0 +1,7 @@
+/* esto es una clase, comentario en block+*/
+class Saludo{
+// esto es un metodo, comentario en linea
+	void saludar(){
+		System.out.println("Hola de nuevo desde java oop");
+	}
+}
