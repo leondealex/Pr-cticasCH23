a35c3b469ae8868a821b08ad3538431a1f75e134
leondealex <deleonavalosa@gmail.com>
2023-03-28 17:21:33 -0600

    JAVA WEB

---
+++ JAVA-WEB/JW-02/ecommerce/src/main/java/com/generation/ecommerce/model/Producto.java
@@ -0,0 +1,70 @@
+package com.generation.ecommerce.model;
+
+public class Producto {
+	
+	//Atributos
+	private Long id;
+	private String nombre;
+	private String descripcion;
+	private String URL_Imagen;
+	private double precio;
+
+	//Constructor vacío para el jackson (serializar y deserializar un objeto JAVA a JSON)
+	public Producto() {
+		
+	}
+	
+	//Constructor con todos los campos
+	public Producto(Long id, String nombre, String descripcion, String URL_Imagen, double precio) {
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.URL_Imagen = URL_Imagen;
+		this.precio = precio;
+	}
+	
+	//Getters & Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getURL_Imagen() {
+		return URL_Imagen;
+	}
+
+	public void setURL_Imagen(String uRL_Imagen) {
+		URL_Imagen = uRL_Imagen;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	
+	
+}
